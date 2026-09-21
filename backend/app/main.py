70128472a66ce72d59ad9d933dad21d00f4770c5
palak7931-beep <palak7931@gmail.com>
2026-09-21 16:10:13 +0530

    Fix production CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -18,18 +18,19 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="ResearchOS API")
 
-# CORS
+#cores
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://research-mate-ai-one.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 app.include_router(auth_router)
 app.include_router(upload_router)
 app.include_router(paper_router)
